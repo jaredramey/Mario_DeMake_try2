7b322e7fc73370e325a2ad149d5a194de5659e33
Jared Ramey <jared.ramey@students.aie.edu.au>
2016-01-04 22:31:28 -0800

    Work on Boss level
    + Added weighted block prefab
    + Added weighted block control script
    + Added boss layer
    + Added lever object
    
    TODO
    = Edit item spawn script to fit spawning an item at XYZ coords instead
    of right on top of gameobject
    = Create animation for lever being used
    = Edit boss to take damage from weighted block instead of player
    = Test and tweak new boss fight
    
    TODO Cont.
    = Redo levels for better temp release
    = Create world map for level navigation
    = Add pause menu (?)
    = Make saving game a smoother process

--- Assets/Scripts/WeightControl.cs
+++ Assets/Scripts/WeightControl.cs
@@ -1,15 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WeightControl : MonoBehaviour {
+public class WeightControl : MonoBehaviour
+{
+    /*
+     * Simple script that is used to check if this weight has hit
+     * lady killer and if so destroy the game object. 
+     * Will probably model a script after this later on for blocks
+     * that will kill enemies in regular world levels.
+     */
+    private GameObject ladyKiller;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float bossCheckRadius;
+    public bool hitBoss;
+    public Transform bossCheck;
+    public LayerMask whatIsBoss;
+
+    public float destuctionDelay;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Fill in later if needed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get boss object
+        ladyKiller = GameObject.Find("ladyKiller");
+
+        //check to see if the boss is under the wieght
+        hitBoss = Physics2D.OverlapCircle(bossCheck.position, bossCheckRadius, whatIsBoss);
+
+        //Check to see if object destruction is needed
+        ObjectStatusCheck();
+    }
+
+    void ObjectStatusCheck()
+    {
+        if(hitBoss)
+        {
+            //If the block has hit the boss then destroy the block
+            Destroy(gameObject, destuctionDelay);
+        }
+    }
 }
